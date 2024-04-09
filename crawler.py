f3b3e04fd6daebf8210cbb4eb4357453921c5fd0
hey-yujin <yujinbaek@kaist.ac.kr>
2024-04-09 12:13:34 +0900

    minor fix

--- crawler.py
+++ crawler.py
@@ -27,10 +27,10 @@ def read_yaml(fname):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_filepath", default="./configs/iclr_2024.yaml")
-    parser.add_argument("--headless", action="store_true", help="Crawl with headless mode.")
     parser.add_argument("--all", action="store_true", help="All selections (oral, spotlight, poster) will be crawled.")
     parser.add_argument("--selection", default="oral")
     parser.add_argument("--max_wait", default=60, type=int, help="Maximum wait time (in seconds) until the page is fully loaded.")
+    parser.add_argument("--headless", action="store_true", help="Crawl with headless mode.")
     return parser.parse_args()
 
 def get_keywords(web_element: WebElement) -> str:
@@ -142,10 +142,20 @@ if __name__ == "__main__":
     else:
         options = None
 
-    web_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    web_driver = webdriver.Chrome(
+        service=ChromeService(
+            ChromeDriverManager().install()
+        ), 
+        options=options,
+    )
 
-    crawler = OpenreviewCrawler(config=config, web_driver=web_driver, max_wait=args.max_wait)
+    crawler = OpenreviewCrawler(
+        config=config, 
+        web_driver=web_driver, 
+        max_wait=args.max_wait,
+    )
     
     for selection in crawling_scope:
         crawler.crawl(selection)
 
+ 
