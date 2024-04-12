8a458aed2fe62723245e92f8ef7f7981b967bad7
hey-yujin <yujinbaek@kaist.ac.kr>
2024-04-12 14:49:49 +0900

    reformat using black

--- crawler.py
+++ crawler.py
@@ -19,7 +19,9 @@ def get_keywords(web_element: WebElement) -> str:
     sections = web_element.find_elements(By.XPATH, ".//div/div/strong")
     for idx, section in enumerate(sections, start=1):
         if section.text.strip() == "Keywords:":
-            keywords = web_element.find_element(By.XPATH, f".//div/div[{idx}]/span").text.strip()
+            keywords = web_element.find_element(
+                By.XPATH, f".//div/div[{idx}]/span"
+            ).text.strip()
             return keywords
     return " "
 
@@ -35,27 +37,37 @@ class OpenreviewCrawler:
 
         paper_base_info = web_element.find_element(By.TAG_NAME, "a")
         data["title"] = paper_base_info.text.strip()
-        data["link"] = paper_base_info.get_attribute("href").strip().replace("forum", "pdf")
+        data["link"] = (
+            paper_base_info.get_attribute("href").strip().replace("forum", "pdf")
+        )
 
         # Get authors info
-        data["authors"] = web_element.find_element(By.CLASS_NAME, "note-authors").text.strip()
-        
+        data["authors"] = web_element.find_element(
+            By.CLASS_NAME, "note-authors"
+        ).text.strip()
+
         # Open up the details to get keywords (> Show details)
         paper_detailed_info = web_element.find_element(By.CLASS_NAME, "collapse-widget")
-        if index == 0: # to avoid ElementClickInterceptedException
-            paper_detailed_info.find_element(By.TAG_NAME, "a").send_keys(Keys.CONTROL + Keys.HOME)
+        if index == 0:  # to avoid ElementClickInterceptedException
+            paper_detailed_info.find_element(By.TAG_NAME, "a").send_keys(
+                Keys.CONTROL + Keys.HOME
+            )
 
         page_loaded = False
         while not page_loaded:
             try:
-                condition = EC.element_to_be_clickable(paper_detailed_info.find_element(By.TAG_NAME, "a"))
+                condition = EC.element_to_be_clickable(
+                    paper_detailed_info.find_element(By.TAG_NAME, "a")
+                )
                 WebDriverWait(self.web_driver, self.max_wait).until(condition)
                 page_loaded = True
             except:
                 time.sleep(1)
 
         try:
-            show_or_hide_details_button = paper_detailed_info.find_element(By.TAG_NAME, "a")
+            show_or_hide_details_button = paper_detailed_info.find_element(
+                By.TAG_NAME, "a"
+            )
             if show_or_hide_details_button.text.strip() == "Show details":
                 show_or_hide_details_button.click()
                 time.sleep(1)
@@ -65,45 +77,47 @@ class OpenreviewCrawler:
             data["keywords"] = get_keywords(paper_detailed_info)
 
         return data
-    
+
     def crawl(self, selection: str):
         main_url = self.config["navigation_paths"]["base_url"].format(
-            selection=selection,
-            **self.config["conference"]
+            selection=selection, **self.config["conference"]
         )
         self.web_driver.get(main_url)
         print(main_url)
 
-        # Explicitly wait until the entire page has fully loaded 
+        # Explicitly wait until the entire page has fully loaded
         # (indicator: right-arrow (>) located at the bottom)
 
         next_page_xpath = self.config["navigation_paths"]["next_page"].format(
-            selection=selection, 
-            page_indicator=self.config["page_indicators"][selection]
+            selection=selection,
+            page_indicator=self.config["page_indicators"][selection],
         )
         condition = EC.presence_of_element_located((By.XPATH, next_page_xpath))
         WebDriverWait(self.web_driver, self.max_wait).until(condition)
 
         # Set output configs
-        save_dir = self.config["save_info"]["save_dir"].format(**self.config["conference"])
+        save_dir = self.config["save_info"]["save_dir"].format(
+            **self.config["conference"]
+        )
         os.makedirs(save_dir, exist_ok=True)
-        save_filename = self.config["save_info"]["save_filename"].format(selection=selection)
+        save_filename = self.config["save_info"]["save_filename"].format(
+            selection=selection
+        )
         save_filename = os.path.join(save_dir, save_filename)
 
         with open(save_filename, "a", encoding="utf-8") as f:
             while True:
-                papers_xpath = self.config["navigation_paths"]["papers"].format(selection=selection)
+                papers_xpath = self.config["navigation_paths"]["papers"].format(
+                    selection=selection
+                )
                 papers = self.web_driver.find_elements(By.XPATH, papers_xpath)
                 for index, paper in tqdm(enumerate(papers), total=len(papers)):
                     data = self.get_paper_details(paper, index)
                     f.write(json.dumps(data, ensure_ascii=False) + "\n")
                 try:
                     self.web_driver.find_element(By.XPATH, next_page_xpath).click()
-                    time.sleep(20)               
+                    time.sleep(20)
 
                 except:
                     print("No next page, exit")
                     break
-
-
- 
--- main.py
+++ main.py
@@ -8,17 +8,30 @@ import yaml
 
 from crawler import OpenreviewCrawler
 
+
 def read_yaml(fname):
     with open(fname, "r") as f:
         return yaml.full_load(f)
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_filepath", default="./configs/iclr_2024.yaml")
-    parser.add_argument("--all", action="store_true", help="All selections (oral, spotlight, poster) will be crawled.")
+    parser.add_argument(
+        "--all",
+        action="store_true",
+        help="All selections (oral, spotlight, poster) will be crawled.",
+    )
     parser.add_argument("--selection", default="oral")
-    parser.add_argument("--max_wait", default=60, type=int, help="Maximum wait time (in seconds) until the page is fully loaded.")
-    parser.add_argument("--headless", action="store_true", help="Crawl with headless mode.")
+    parser.add_argument(
+        "--max_wait",
+        default=60,
+        type=int,
+        help="Maximum wait time (in seconds) until the page is fully loaded.",
+    )
+    parser.add_argument(
+        "--headless", action="store_true", help="Crawl with headless mode."
+    )
     return parser.parse_args()
 
 
@@ -32,7 +45,7 @@ if __name__ == "__main__":
         crawling_scope = config["conference"]["selections"]
     else:
         crawling_scope = [args.selection]
-    
+
     # Set a webdriver
     if args.headless:
         options = webdriver.ChromeOptions()
@@ -41,19 +54,15 @@ if __name__ == "__main__":
         options = None
 
     web_driver = webdriver.Chrome(
-        service=ChromeService(
-            ChromeDriverManager().install()
-        ), 
+        service=ChromeService(ChromeDriverManager().install()),
         options=options,
     )
 
     crawler = OpenreviewCrawler(
-        config=config, 
-        web_driver=web_driver, 
+        config=config,
+        web_driver=web_driver,
         max_wait=args.max_wait,
     )
-    
+
     for selection in crawling_scope:
         crawler.crawl(selection)
-
- 
